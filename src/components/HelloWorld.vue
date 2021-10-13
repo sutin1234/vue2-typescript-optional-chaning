7efe874d6577e842e1d74cee4c1cc13174e148dc
Sutin <poly.dev2018@gmail.com>
2021-10-13 23:32:25 +0700

    added support optional chaning

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      {{ address }}
+    </p>
+    <p>Locations: {{ address?.locations }}</p>
+    <p>Locations: {{ address?.locations ?? 'no locations' }}</p>
+    <p>
+      Lat, Lng: {{ address?.locations?.lat }}, {{ address?.locations?.lng }}
+    </p>
+    <p>
+      Lat, Lng: {{ address?.locations?.lat ?? 'no Lat' }},
+      {{ address?.locations?.lng ?? 'no Lng' }}
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+interface Location {
+  lat: string
+  lng: string
+}
+interface Address {
+  locations: Location
+  city: string
+}
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() msg!: string
+  @Prop() address!: Address
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.hello {
+  font-size: 20px;
+}
+</style>
---
+++ src/views/Test.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Test</h1>
+    <HelloWorld :msg="msg" :address="address" />
+  </div>
+</template>
+
+<script lang="ts">
+import HelloWorld from '@/components/HelloWorld.vue'
+import { Vue, Component } from 'vue-property-decorator'
+@Component({
+  components: {
+    HelloWorld: HelloWorld
+  }
+})
+export default class Test extends Vue {
+  msg = 'Hello Vue2 Typescript'
+  address = {
+    locaytions: {},
+    city: 'BKK'
+  }
+}
+</script>
+
+<style scoped>
+div {
+  padding: 20px;
+}
+</style>
